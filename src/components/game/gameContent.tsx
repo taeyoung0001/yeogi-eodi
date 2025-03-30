4f7ba03ac6d347504f4a2ce72684dfa4c7c13d94
taeyoung <her020212@naver.com>
2025-03-30 11:49:23 +0900

    [refactor] Image 값 수정

--- src/components/game/gameContent.tsx
+++ src/components/game/gameContent.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import { Gamedata } from "@/type/datatype";
-import Image from "next/image";
 
 import AnswerExBox from "./answerExBox";
 
@@ -10,12 +10,7 @@ export default function GameContent(props: Gamedata) {
     <div>
       <h2>이 국기는 어디 나라 국기 일까요?</h2>
       <p>진짜 정답 : {data?.country_nm}</p>
-      <Image
-        src={data?.download_url}
-        alt={"나라국기"}
-        width={300}
-        height={200}
-      />
+      <img src={data?.download_url} alt={"나라국기"} />
       <div>
         <AnswerExBox answerEx={answerEx} data={data} />
       </div>
